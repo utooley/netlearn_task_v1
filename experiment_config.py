ce53dcc962e22b4b4f3ef185357541324cb2845f
Ursula Tooley <utooley@gmail.com>
2020-02-25 11:03:44 -0500

    removed extra parameters left over:

--- experiment_config.py
+++ experiment_config.py
@@ -10,14 +10,3 @@ EXPOSURE_STEPS = 150#change this to change number of trials
 KS_CUTOFF= 0.135#how close does it have to be to a uniform distribution?
 MIN_TRANS_EDGE=7 #what is the range for number of times cross the transition edge?
 MAX_TRANS_EDGE=13
-
-### OFFLINE TEST
-OFFLINE_ACCURACY_BONUS = 1.0
-OFFLINE_BONUS_THRESHOLD = 0.6
-
-RATE_PER_MINUTE = 0.10  # dollars per minute
-EXPOSURE_BONUS = 1.0
-OFFLINE_BONUS = 1.0
-TEST_BONUS = 0.0
-
-MAX_BONUS = 10.0
